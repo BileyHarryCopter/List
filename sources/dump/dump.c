fb8cc811781278d3fa79ffc32757030c71a07aae
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-04-01 02:19:40 +0300

    dum

--- sources/dump/dump.c
+++ sources/dump/dump.c
@@ -1,28 +1,30 @@
 #include "../list/list.h"
+#include "../includes/init.h"
+
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
                         //  REALISATION OF DUMPING  //
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 
 //TODO: add a mode of разрешения выводимого файла
 
-char * System_Dump (List_t list, char * name_file)
+char * SystemDump (List_t *list, char * name_file)
 {
     char * Fname = (char *) calloc (FLEXSIZE, sizeof (char));
     strcat (Fname, "dot ");
     strcat (Fname, name_file);
     strcat (Fname, " -T png -o sources/dump/");
-    strcat (Fname, list.name);
+    strcat (Fname, list->name);
     strcat (Fname, "_dump.png");
 
     return Fname;
 }
 
-int Graph_Dump (List_t list)
+int GraphDump (List_t *list)
 {
-    assert (list.name);
+    assert (list->name);
     char *name_file = (char *)calloc (2*NAMESIZE, sizeof (char));
     strcpy (name_file, "sources/dump/");
-    strcat (name_file, list.name);
+    strcat (name_file, list->name);
     strcat (name_file, "_dump.dot");
     assert (name_file);
 
@@ -43,38 +45,38 @@ int Graph_Dump (List_t list)
             "\t\tnode [shape = record, style = \"filled\", fillcolor = \"teal\", fontcolor = \"white\"];\n"
             "\t\tedge [maxlen = 2, penwidth = 1, arrowhead = \"empty\", weight = 0, dir = \"both\"];\n"
             "\t\t\tl_elem_%d [label = \"<name> FICT | <next> NEXT: %d | <prev> PREV: %d | <data> DATA: null\", fillcolor = \"darkslategray\"];\n",
-            0, list.next[0], list.prev[0]);
-    for (int i = 1; i < list.capacity; i++)
+            0, list->next[0], list->prev[0]);
+    for (int i = 1; i < list->capacity; i++)
     {
         fprintf (file, "\t\t\tl_elem_%d [label = \"<name> NODE: %d | <next> NEXT: %d | <prev> PREV: %d | <data> DATA: %.2lf\"];\n",
-                        i, i, list.next[i], list.prev[i], list.data[i]);
+                        i, i, list->next[i], list->prev[i], list->data[i]);
     }
 
     fprintf (file, "\t\tl_elem_0");
-    for (int i = 1; i < list.capacity; i++)
+    for (int i = 1; i < list->capacity; i++)
     {
         fprintf (file, "->l_elem_%d", i);
     }
     fprintf (file, " [weight = 5, style = \"invisible\", arrowhead = \"none\", dir = \"forward\"];\n\n");
 
-    if (list.insertion != 0)
+    if (list->insertion != 0)
     {
-        for (int i = 0; i < list.capacity; i++)
+        for (int i = 0; i < list->capacity; i++)
         {
             if (i == 0)
             {
-                fprintf (file, "\t\tl_elem_%d:se -> l_elem_%d:sw;\n", i, list.next[i]);
-                fprintf (file, "\t\tl_elem_%d:ne -> l_elem_%d:nw;\n", i, list.prev[i]);
+                fprintf (file, "\t\tl_elem_%d:se -> l_elem_%d:sw;\n", i, list->next[i]);
+                fprintf (file, "\t\tl_elem_%d:ne -> l_elem_%d:nw;\n", i, list->prev[i]);
                 continue;
             }
-            else if (list.prev[i] == -1 || list.next[i] == 0)
+            else if (list->prev[i] == -1 || list->next[i] == 0)
                 continue;
-            if (list.next[i] < i)
+            if (list->next[i] < i)
             {
-                fprintf (file, "\t\tl_elem_%d:nw -> l_elem_%d:ne;\n", i, list.next[i]);
+                fprintf (file, "\t\tl_elem_%d:nw -> l_elem_%d:ne;\n", i, list->next[i]);
                 continue;
             }
-            fprintf (file, "\t\tl_elem_%d:se -> l_elem_%d:sw;\n", i, list.next[i]);
+            fprintf (file, "\t\tl_elem_%d:se -> l_elem_%d:sw;\n", i, list->next[i]);
         }
     }
 
@@ -87,7 +89,7 @@ int Graph_Dump (List_t list)
             "\t\tedge\t[maxlen = 1 , penwidth = 1, arrowhead = \"empty\"];\n"
             "\t\tmain\t[label = \"{NAME:  %s} | {CAPACITY:  %u} | {INSERT:  %d} | {FREE:  %d} | {FICT:  %d}\"];\n"
             "\t}\n\n",
-            list.name, list.capacity, list.insertion, list.free, list.fict);
+            list->name, list->capacity, list->insertion, list->free, list->fict);
 
     fprintf (file,
             "\tsubgraph MAIN\n"
@@ -102,7 +104,7 @@ int Graph_Dump (List_t list)
 
 // TODO: nothing works
 // why???
-    char * comdump = System_Dump (list, name_file);
+    char * comdump = SystemDump (list, name_file);
     printf ("FILE NAME: %s\n", comdump);
     //  system (comdump);
     free (comdump);
---
+++ sources/includes/includes.h
@@ -0,0 +1,8 @@
+#include <assert.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//  still is not for unsing
