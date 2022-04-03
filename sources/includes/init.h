dbdb455d349374932217021c781ec87b633662e7
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-04-03 16:47:44 +0300

    clean an initialization of the structer

--- sources/includes/init.h
+++ sources/includes/init.h
@@ -1,21 +1,20 @@
 #ifndef INIT
 #define INIT
+
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
             //  incapsulation an initialization of the structer //
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 struct List_t {
-  data_t       *data;
-  int          *next;
-  int          *prev;
-  char         *name;
-  unsigned     capacity;
-  int          fict;
-  int          free;
-  int          insertion;
-  FILE         *dampfile;
+    data_t       *data;
+    int          *next;
+    int          *prev;
+    char         *name;
+    unsigned     capacity;
+    int          fict;
+    int          free;
+    int          insertion;
 };
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
 //===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*//
-
 
 #endif
